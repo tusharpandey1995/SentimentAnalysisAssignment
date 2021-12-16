73b64e90abb15f1a103472fa4895b1570e821c19
tusharpandey1995 <tusharpandey1995@gmail.com>
2021-12-16 19:09:32 +0000

    code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
+import sys
 # Libraries for feature engineering
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
@@ -9,15 +11,20 @@ model = pickle.load(open('best_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('/templates/index.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    str_features = [str(x) for x in request.form.values()]
-    final_features = [np.array(str_features)]
+    # str_features = [str(x) for x in request.fm.values()]
+    # print(str_features)
+    tv = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0, ngram_range=(1,2),sublinear_tf=True)
+    str_features = pd.DataFrame([np.array(request.form['inputText'])])
+    final_features = tv.fit_transform(str_features)
+    # final_features = 
+    print(final_features)
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
@@ -27,3 +34,4 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    app.config["DEBUG"]=True
