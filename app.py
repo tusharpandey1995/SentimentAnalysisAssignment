3c28b808cb9cc75ddc888c90708e00090c77e058
tusharpandey1995 <tusharpandey1995@gmail.com>
2021-12-17 04:51:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,15 @@ def predict():
     vect = tv.transform(data).toarray()
     prediction = model.predict(vect)
     confidence = model.predict_proba(vect)
-    cofd = round((confidence.flat[0]*100),2)
+    
     
 
     if prediction[0]:
         output = 'Positive'
+        cofd = round((confidence.flat[1]*100),2)
     else:
         output = 'Negative'
+        cofd = round((confidence.flat[0]*100),2)
 
     return render_template('Index.html', prediction_text='Sentiment Polarity: {}'.format(output), confidence_text='Confidence: {}'.format(cofd))
 
