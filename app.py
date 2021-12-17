742c3cf31992c010c64708d10220d59183a45eed
tusharpandey1995 <tusharpandey1995@gmail.com>
2021-12-17 01:55:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 app = Flask(__name__)
 model = pickle.load(open('best_model.pkl', 'rb'))
 
-print('+++++++App Running+++++++')
-
 @app.route('/')
 def home():
 
@@ -23,7 +21,7 @@ def predict():
     '''
     # str_features = [str(x) for x in request.fm.values()]
     # print(str_features)
-    print('Inside predict function!!!!!!!!!!!!')
+    print('+++++++App Running from predict+++++++')
     tv = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0, ngram_range=(1,2),sublinear_tf=True)
     str_features = pd.DataFrame([np.array(request.form['inputText'])])
     final_features = tv.fit_transform(str_features)
