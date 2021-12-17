3f0ae0754c007a0583a8736fda5fed8275b19425
tusharpandey1995 <tusharpandey1995@gmail.com>
2021-12-17 02:36:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def predict():
     tv = TfidfVectorizer(use_idf=True, min_df=0.0, max_df=1.0, ngram_range=(1,2),sublinear_tf=True)
 #     str_features = pd.DataFrame([request.form['inputText']],columns=['Review'],ignore_index=True)
 #     print('+++++++str_features+++++++',str_features.Review)
-    final_features = tv.fit_transform(request.form['inputText'])
+    final_features = tv.fit_transform([request.form['inputText']])
     # final_features = 
     print(final_features)
     prediction = model.predict(final_features)
